403c04f93e4111b6086afb3679a8e9d95804f5e0
DET171 <mrgeek484@gmail.com>
2022-03-12 13:11:11 +0800

    init

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="ui container" v-html="text">
+  </div>
+</template>
+
+<script setup lang="ts">
+import path from 'path';
+const { $loadMd } = useNuxtApp();
+
+const text = $loadMd('/home/yc/Documents/code/ec3-gang.github.io/pages/index.md')
+</script>
